362a4a557663e6a51f09ff106b8aa101824f810c
bsmita <bsmita@us.ibm.com>
2020-07-16 16:47:13 -0500

    Asset scripts

--- lib/vapi/DnnScripts.py
+++ lib/vapi/DnnScripts.py
@@ -19,6 +19,9 @@
 
 
 import logging as logger
+import os
+import requests
+from requests.auth import HTTPDigestAuth
 
 
 class DnnScripts:
@@ -26,18 +29,24 @@ class DnnScripts:
     def __init__(self, server):
         self.server = server
 
-    def create(self, name, **kwargs):
+    def create(self, name, inputfile, **kwargs):
         """ Create a new Dnn script.
 
-        :param name   -- name for the new datasets
+        :param name   -- name for the new dnn script
         :param kwargs -- optional fields for the creation payload. See
-                        "POST /datasets" API documentation for details"""
+                        "POST /dnn-scripts" API documentation for details"""
 
-        uri = "/datasets"
-        body = {"name": name}
-        body.update(kwargs)
+        uri = "/dnn-scripts"
+        #f = open('/Users/bsmita123/goodCustomFile.zip', 'rb')
+        #file = {'files': open('/Users/bsmita123/goodCustomFile.zip', 'rb')}
+        #url = "https://aprilmin3.aus.stglabs.ibm.com/visual-insights/api/dnn-scripts"
+        #r = requests.post(url, auth=HTTPDigestAuth('admin', 'passw0rd'), files={"archive": ("test.zip", file)})
+        #body = {'name': name, 'files': open('/Users/bsmita123/goodCustomFile.zip', 'rb')}
+        #print(body)
+        #body.update(kwargs)
 
-        return self.server.post(uri, json=body)
+        #return self.server.post(uri, json=body)
+        return self.server.post(uri, files = inputfile, data = {'name': name})
 
     def report(self, **kwargs):
         """ Get a list of all dnn scripts
@@ -60,71 +69,19 @@ class DnnScripts:
         return self.server.put(uri, json=kwargs)
 
     def delete(self, dnnid):
-        """ Delete the indicated dataset
-
-        :param dsid  -- UUID of the targeted dataset"""
-
-        uri = f"/dnn-scripts/{dnnid}"
-        return self.server.delete(uri)
-
-    def show(self, dsid):
-        """ Get details of the indicated dataset
+        """ Delete the indicated DNN script
 
         :param dnnid  -- UUID of the targeted DNN script"""
 
         uri = f"/dnn-scripts/{dnnid}"
+        response = self.server.delete(uri)
+        return response
+
+    def show(self, dnnname):
+        """ Get details of the indicated Dnn Script
+
+        :param dnnname  -- Name of the targeted DNN script"""
+
+        uri = f"/dnn-scripts/{dnnname}"
         return self.server.get(uri)
 
-    def import_dataset(self, file_path):
-        """ Imports an exported dataset zip file into the server.
-
-        :param file_path -- path to zip file to upload"""
-
-        file = {'files': open(file_path, 'rb')}
-
-        uri = "/datasets/import"
-        return self.server.post(uri, files=file)
-
-    def export(self, dsid, filename=None, status_callback=None, raw=False):
-        """ exports the indicated dataset into the specified file
-
-        :param dsid   UUID of the dataset to be exported
-        :param filename  name of the file into which the dataset is saved.
-                        If no filename is provided, it will be taken from the response
-                        header. If no name is present in the header, a FileNotFoundError
-                        is raised. It is best to provide a filename
-        :param status_callback  function to be called if progress updates are desired.
-                        Status callbacks are made every 50 megabytes saved. Three parameters
-                        are provided with the callback
-                          - name of the file being saved into
-                          - number of callbacks made (starting at 1)
-                          - total number of bytes saved so far.
-        :param raw   if True, causes raw metadata DB contents for the dataset to be included.
-
-        :return: returns the absolute path of the saved file. """
-
-        params = {"raw": raw}
-        uri = f"/datasets/{dsid}/export"
-        logger.debug(F"export: URL={uri}; params={params}; filename={filename}")
-
-        self.server.get(uri, params=params, stream=True)
-        if self.server.raw_rsp().ok:
-            filename = self.server.save_file(filename, status_callback)
-        else:
-            filename = None
-
-        return filename
-
-    def clone(self, dsid, name):
-        """ Handles creation of dataset clone request.
-
-        :param dsid  UUID of the dataset to clone.
-        :param name  name of the new dataset."""
-
-        uri = f"/datasets/{dsid}/action"
-        body = {
-            "action" : "clone",
-            "name": name
-        }
-
-        return self.server.post(uri, json=body)
